cafb771b28a4b111634d8ecc5bf6499fad85444b
Bektur Batyrbekov <batyrbekovbektur0@gmail.com>
2023-12-14 18:58:29 +0600

    Initialized TS

--- add.js
+++ add.js
@@ -1,1 +1,18 @@
-
+"use strict";
+function chessBoard() {
+    const limit = 8;
+    let result = '';
+    for (let i = 0; i < limit; i++) {
+        for (let j = 0; j < limit; j++) {
+            if (i % 2 !== 0) {
+                result += j % 2 === 0 ? '  ' : '██';
+            }
+            else {
+                result += j % 2 === 0 ? '██' : '  ';
+            }
+        }
+        result += '\n';
+    }
+    return result;
+}
+console.log(chessBoard());
